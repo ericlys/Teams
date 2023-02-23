195a48bb9e78340fdc6b058268f3adea6712731a
Ericlys Moreira <ericlysm@gmail.com>
2023-02-23 19:07:11 -0300

    feat: listempty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Headers';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
@@ -14,7 +19,20 @@ export function Groups() {
         subtitle='jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do Ignite' onPress={() => console.log('teste')}/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primerira turma?"/>
+        )}
+      />
+
     </Container>
   );
 }
