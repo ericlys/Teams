a034a762ccf3b67b421a99844a69711cf321cb79
Ericlys Moreira <ericlysm@gmail.com>
2023-02-23 00:02:43 -0300

    chore: add styled component add theme setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { Groups } from '@screens/Groups';
 
+import theme from '@theme/index';
+
+import { ThemeProvider } from 'styled-components/native'
+
 export default function App() {
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+      <Groups/>
+    </ThemeProvider>
   );
 }
