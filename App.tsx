aa7249d1e538fe80be54c016e45e141af70c6d66
Ericlys Moreira <ericlysm@gmail.com>
2023-02-23 00:10:25 -0300

    chore: add external font

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { Groups } from '@screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from '@theme/index';
 
 import { ThemeProvider } from 'styled-components/native'
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoader] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      {fontsLoader ? <Groups/> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
