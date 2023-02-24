ba78132a5eff3ae93ed55fc81e64af11558e8bbf
Ericlys Moreira <ericlysm@gmail.com>
2023-02-24 20:28:08 -0300

    chore: handling exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { Container, Content, Icon } from "./styles";
 
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 import { Header } from "@components/Headers";
 import { Highlight } from "@components/Highlight";
@@ -15,11 +17,21 @@ export function NewGroup() {
   const navigation = useNavigation();
 
   async function handleNew() {
+    if(group.trim().length === 0) {
+      return Alert.alert('Novo Grupo', 'informe o nome da turma.');
+    }
+
     try {
       await groupCreate(group);
-      navigation.navigate('players', { group })
+      navigation.navigate('players', { group });
+
     }catch (error) {
-      console.log(error);
+      if(error instanceof AppError){
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo','Não foi possível criar um novo grupo.');
+        console.log(error);
+      }  
     }
   }
 
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppError } from "@utils/AppError";
 
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
@@ -6,6 +7,12 @@ import { groupsGetAll } from "./groupsGetAll";
 export async function groupCreate(newGroup: string) {
   try {
     const storage = await groupsGetAll();
+
+    const groupAlreadyExists = storage.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError('Já existe um grupo cadastrado com esse nome.');
+    }
 
     await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify([...storage, newGroup]));
 
