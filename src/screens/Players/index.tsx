78567217629ef156329f138971c89ebdab070768
Ericlys Moreira <ericlysm@gmail.com>
2023-02-24 23:24:27 -0300

    chore: storing player

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,9 +7,12 @@ import { Input } from "@components/Input";
 import { ListEmpty } from "@components/ListEmpty";
 import { PlayCard } from "@components/PlayerCard";
 import { useRoute } from "@react-navigation/native";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
+import { AppError } from "@utils/AppError";
 
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
@@ -18,11 +21,35 @@ type RouteParams = {
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([]);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if(newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+
+    } catch(error) {
+      if(error instanceof AppError){
+        Alert.alert('Nova pessoa', error.message);
+      }else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possivel adicionar.')
+      }
+    }
+  }
 
   return(
     <Container>
@@ -37,11 +64,13 @@ export function Players() {
         <Input
           placeholder="Nome da pessoa"
           autoCorrect={false}
-          />
+          onChangeText={setNewPlayerName}
+        />
           
         <ButtonIcon 
           icon="add"
-          />
+          onPress={handleAddPlayer}
+        />
       </Form>
 
       <HeaderList>
